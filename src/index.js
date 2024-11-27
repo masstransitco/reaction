0b01786692ad01086d5ec6c7b2a78144b65ae06c
Neuter <mark@air.city>
2024-11-27 16:16:23 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 // src/index.js
 
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+
 import { AppProvider } from "./context/AppContext";
+
 import ErrorBoundary from "./components/ErrorBoundary";
 
+
 // Import ArcGIS CSS
-import "arcgis-js-api/css/main.css"; // Ensure this path is correct based on your setup
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Failed to find the root element");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
